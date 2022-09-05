ce8a8f1494a6830fee2c6da4d7c6b6e34d59a289
Girdhar Agarwal <agarwalgirdhar2002@gmail.com>
2022-09-05 23:56:06 +0530

    database connection added

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const mongoose=require("mongoose");
 const app=express();
 
 dotenv.config({path : './config.env'});
-require("./db/conn.js");
+
+const CONNECTION_URL=process.env.CONNECTION_URL;
+// console.log(CONNECTION_URL);
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log("Database connected"))
+.catch((error) => console.log(error));
+
 app.use(express.json());
 app.use(express.static("client"));
 app.use(require("./router/auth"));
