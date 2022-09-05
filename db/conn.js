a5ba3e1954a79e139b8de323d5f87042bb0f5239
Girdhar Agarwal <agarwalgirdhar2002@gmail.com>
2022-09-05 23:38:35 +0530

    heroku

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
-
+const dotenv=require("dotenv");
 const CONNECTION_URL=process.env.CONNECTION_URL;
 // console.log(CONNECTION_URL);
 
-mongoose.connect("mongodb+srv://Ag_Girdhar12:lovesanudhar@cluster0.k7nf2.mongodb.net/Forum?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("Database connected"))
 .catch((error) => console.log(error));
