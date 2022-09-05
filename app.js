141ad60f325f33ed5cd80f42551abe6e0bc60ae0
Girdhar Agarwal <agarwalgirdhar2002@gmail.com>
2022-09-06 00:22:26 +0530

    database connection added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config({path : './config.env'});
 const CONNECTION_URL=process.env.CONNECTION_URL;
 // console.log(CONNECTION_URL);
 
-mongoose.connect("mongodb+srv://Ag_Girdhar12:lovesanudhar@cluster0.k7nf2.mongodb.net/Forum?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("Database connected"))
 .catch((error) => console.log(error));
 
