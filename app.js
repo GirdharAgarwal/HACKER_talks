daf9d333227028519452a6d5e9f4f2a290cc21a2
Girdhar Agarwal <agarwalgirdhar2002@gmail.com>
2022-09-06 12:03:26 +0530

    connection-url hide

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const mongoose=require("mongoose");
 const app=express();
 
 dotenv.config({path : './config.env'});
-
-const CONNECTION_URL=process.env.CONNECTION_URL;
-// console.log(CONNECTION_URL);
-
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log("Database connected"))
-.catch((error) => console.log(error));
+require("./db/conn");
 
 app.use(express.json());
 app.use(express.static("client"));
